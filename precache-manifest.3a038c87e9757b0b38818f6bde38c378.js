e9d9cb09f6bc43b9947baff37594625f315a7090
JPL1 <JPL1@users.noreply.github.com>
2019-10-27 19:56:51 +0000

    Add updated website files via upload

---
+++ precache-manifest.3a038c87e9757b0b38818f6bde38c378.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d216d27a041396a2169779db202357",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe700082d613bf64a3d4",
+    "url": "./static/css/main.5aad7bf8.chunk.css"
+  },
+  {
+    "revision": "67be4232073a0bb54841",
+    "url": "./static/js/2.eaf38937.chunk.js"
+  },
+  {
+    "revision": "fe700082d613bf64a3d4",
+    "url": "./static/js/main.8229a9a0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4c5d58d273f725fdae169ea2d66953d2",
+    "url": "./static/media/api_flow_revised.4c5d58d2.PNG"
+  },
+  {
+    "revision": "a643ebb89327e3a772b7a94631b11f27",
+    "url": "./static/media/app_auth_flow.a643ebb8.jpg"
+  },
+  {
+    "revision": "f92f7295dfde67589a7c270045fcecbc",
+    "url": "./static/media/data_types.f92f7295.png"
+  },
+  {
+    "revision": "3071f5e44d28ce19b2a44a1413886928",
+    "url": "./static/media/networking_layers.3071f5e4.jpg"
+  },
+  {
+    "revision": "835f0e5c66f6454573fb87263a3e99fa",
+    "url": "./static/media/proof_of_resource.835f0e5c.jpg"
+  },
+  {
+    "revision": "10156824baf8b8df3d4f5830f2d41ddc",
+    "url": "./static/media/resources_and_currency.10156824.jpg"
+  },
+  {
+    "revision": "6a1c4e80926cf3446725bfa77e3e7301",
+    "url": "./static/media/stack.6a1c4e80.jpg"
+  },
+  {
+    "revision": "e48595efd3159f157feb3e372a39dca0",
+    "url": "./static/media/vaults_journey.e48595ef.jpg"
+  },
+  {
+    "revision": "012600c783e1ef3a22c28aa44f7a5997",
+    "url": "./static/media/xor_addresses.012600c7.jpg"
+  },
+  {
+    "revision": "dc6c58cb0fff1414749b91a00ec38f4c",
+    "url": "./static/media/xor_url.dc6c58cb.PNG"
+  }
+]);
