9c62d05bfb89be6cbb957eb2a31b2aca8902af30
JPL1 <JPL1@users.noreply.github.com>
2021-02-08 22:22:40 +0000

    Update Chap13.js
    Change SAFE to Safe, Vault to Node, Safecoin to Safe Network Token

--- src/pages/Chap13.js
+++ src/pages/Chap13.js
@@ -6,11 +6,11 @@ import React from 'react';
 
 const component = () => (
     <div>
-        <h2 id='ch13'>13. The promise of the SAFE Network</h2>
+        <h2 id='ch13'>13. The promise of the Safe Network</h2>
 
 
 
-        <p>The SAFE Network is still in development. While many features and functionalities have already proven themselves under test conditions others, including Safecoin, are still to come.&nbsp; As with any cutting-edge experimental technology, the proof of the pudding is in the eating. But let&rsquo;s assume for a moment that the Network is successful and is widely adopted for various use cases including Internet browsing, IoT connectivity, data security, personal information management, medical records and more.</p>
+        <p>The Safe Network is still in development. While many features and functionalities have already proven themselves under test conditions others, including Safe Network Token, are still to come.&nbsp; As with any cutting-edge experimental technology, the proof of the pudding is in the eating. But let&rsquo;s assume for a moment that the Network is successful and is widely adopted for various use cases including Internet browsing, IoT connectivity, data security, personal information management, medical records and more.</p>
 
         <p>What would that world look like? First, most cyber attack strategies deployed today would be dead in the water. DDoS would not work as the Network would simply route around the affected nodes. Viruses and malware would be extremely limited in their depth of penetration. Ransomware would not raise a single dollar. Cyber attacks aimed at disabling national infrastructure or taking control of a driverless car would be extremely hard to pull off. Medical records and other personal data would be ours and ours alone, to share as we see fit. For users there can be a single sign-on to multiple services. XOR networking with opportunistic caching promises faster speeds, data storage should be extremely cheap and the Network will offer high levels of availability. For developers having a single storage architecture to address has the potential to simplify the systems programmer&rsquo;s job. And data deduplication would allow for both simplicity and resource savings.</p>
 
@@ -22,19 +22,19 @@ const component = () => (
 
         <p>The Internet giants of today would no longer be able to harvest our data without our say so, nor could government spooks eavesdrop over their shoulders. There would be a rebalancing of power from the data-haves to the data have-nots. Censorship would be impossible and data could not be erased. Because the cost of entry will be low and access unrestricted, the ongoing net neutrality debate will end, and neutrality will have won the day.</p>
 
-        <p>People in places with poor or restricted access to information will have those blockers lifted. Some may even make a decent living earning Safecoin. New business models based on consent would spring up in a world where data storage and networking and eventually compute is a commodity, and the world of information will be a much more level playing field.</p>
+        <p>People in places with poor or restricted access to information will have those blockers lifted. Some may even make a decent living earning Safe Network Token. New business models based on consent would spring up in a world where data storage and networking and eventually compute is a commodity, and the world of information will be a much more level playing field.</p>
 
-        <p>Isn&rsquo;t this all a bit idealistic? Well yes of course, but that&rsquo;s the nature of visions. Not everything will work as planned and we need to be hard-headed about that. Techno-utopianism is a dangerous thing. There are both predictable and unpredictable consequences of taking on the status quo, and indeed of deploying any new technology, and not all of them will be positive. Nevertheless, given where we are today, and where we are headed with the IoT, something like the SAFE Network is most definitely needed to redress the power imbalance and to secure the data-driven future.</p>
+        <p>Isn&rsquo;t this all a bit idealistic? Well yes of course, but that&rsquo;s the nature of visions. Not everything will work as planned and we need to be hard-headed about that. Techno-utopianism is a dangerous thing. There are both predictable and unpredictable consequences of taking on the status quo, and indeed of deploying any new technology, and not all of them will be positive. Nevertheless, given where we are today, and where we are headed with the IoT, something like the Safe Network is most definitely needed to redress the power imbalance and to secure the data-driven future.</p>
 
         <h3>Roadmap</h3>
 
-        <p>As with all experimental research-driven projects the rate of progress is unpredictable, and MaidSafe avoid giving hard deadlines. However, there is a roadmap at <a href="https://safenetwork.tech/timeline/">https://safenetwork.tech/timeline/</a> in which the next developments are laid out. The next MVP, SAFE Fleming, will allow anyone with a computer and an internet connection to join the Network.</p>
+        <p>As with all experimental research-driven projects the rate of progress is unpredictable, and MaidSafe avoid giving hard deadlines. However, there is a roadmap at <a href="https://safenetwork.tech/timeline/">https://safenetwork.tech/timeline/</a> in which the next developments are laid out. The next MVP, Safe Fleming, will allow anyone with a computer and an internet connection to join the Network.</p>
 
         <h2>About MaidSafe</h2>
 
 
 
-        <p>MaidSafe is a company founded by David Irvine in 2006 with a mission to provide security and privacy for everyone by building a better digital world. This new platform is the SAFE Network, which is the world&rsquo;s first autonomous and decentralized data network. The Network is made up of the unused hard drive space, processing power and bandwidth of its users. The SAFE Network will include storage, peer-to-peer communications, transactions, Internet functionality and a wide variety of apps to name a few of its features. This paper was written and produced by members of the SAFE Network Forum independently of MaidSafe.&nbsp;</p>
+        <p>MaidSafe is a company founded by David Irvine in 2006 with a mission to provide security and privacy for everyone by building a better digital world. This new platform is the Safe Network, which is the world&rsquo;s first autonomous and decentralized data network. The Network is made up of the unused hard drive space, processing power and bandwidth of its users. The Safe Network will include storage, peer-to-peer communications, transactions, Internet functionality and a wide variety of apps to name a few of its features. This paper was written and produced by members of the Safe Network Forum independently of MaidSafe.&nbsp;</p>
 
         <p>Join the debate at <a href="https://safenetforum.org/">https://safenetforum.org</a>&nbsp;</p>
 
@@ -48,9 +48,11 @@ const component = () => (
 
         <p>Twitter: <a href="https://twitter.com/maidsafe">https://twitter.com/maidsafe</a></p>
 
+        <p>Twitter: <a href="https://twitter.com/maidsafe">https://twitter.com/safenetworktech</a></p>
+
         <p>Reddit: <a href="https://reddit.com/r/safenetwork">https://reddit.com/r/safenetwork</a></p>
 
-        <p>Telegram: <a href="https://t.me/safenetwork">https://t.me/safenetwork</a></p>
+        <p>Telegram: <a href="https://t.me/safenetwork">https://t.me/safenetwork</a></p> 
 </div>
 );
 
