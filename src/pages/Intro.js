c649efbf6fbded6b5d8a33a39bfc8d2aa0a35297
JPL1 <JPL1@users.noreply.github.com>
2021-11-11 09:55:04 +0000

    Update Intro.js

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -6,10 +6,11 @@ const component = () => (
   
         <p>Technological progress is a perpetual process of automation and abstraction. Difficult and complex tasks are made simple by software and machines until they are completely taken for granted. Technology moves on, taking down new blockers in the path to progress as it goes. A good example of this is cloud computing, where once complex server administration tasks have been replaced, from the point of view of the consumer, by point-and-click.</p>
         <p>The Safe (Secure Access For Everyone) Network takes this further, automating the entire network of interconnected machines, making it autonomous, secure, anonymous and capable of&nbsp; storing, protecting and delivering data without any human involvement at all.</p>
-        <p>The Safe Network is the vision of MaidSafe, a Scottish software company working in the field of decentralized computer networking. It is an autonomous peer-to-peer network created by linking together users' computers and smartphones that's designed to solve many of the current technical, managerial and societal problems with centralized networks: a lack of privacy and data security, censorship and the massive consolidation of control by a few powerful actors.&nbsp;</p>
+        <p>The Safe Network is the vision of MaidSafe, a Scottish software company working in the field of decentralized computer networking. It is an autonomous peer-to-peer network created by linking together users' computers and smartphones that's designed to solve many of the current technical, managerial and societal problems exacerbated by centralized networks: a lack of privacy and data security, censorship and the massive consolidation of control by a few powerful actors.&nbsp;</p>
         <p>(In case this sounds familiar, members of MaidSafe acted as advisers for HBO&rsquo;s <i>Silicon Valley</i> TV series in which a startup tries to reinvent the Internet!)</p>
-        <p>The Network is &lsquo;trustless&rsquo;, with no central point of control and no single point of failure. With connectivity and security taken care of, the Network is simple from the point of view of developers, with no need to worry about low-level storage, networking, backups and computing resources. For people using the Network to store and share data and messages the burden of ensuring the security of precious information would be drastically reduced.</p>
-        <p>Anyone with a connected device can join the network anonymously (subject to a resource test) as a provider, and anyone can use the network to store data or peruse public information on it. The Network is thus a platform on which new digital worlds can be constructed.</p>
+        <p>The Network is &lsquo;trustless&rsquo;, with no central point of control and no single point of failure. With connectivity and security taken care of, the Network is simple from the point of view of developers, with the burden of having to worry about low-level storage, networking, backups and computing resources removed. For people using the Network to store and share data and messages the burden of ensuring the security of precious information would be drastically reduced.</p>
+        <p>Anyone with a connected device can use the Network anonymously to store data or peruse public information on it, and anyone (subject to a resource test) can join the Network anonymously as a provider.</p> 
+        <p>The Safe Network is a platform on which new digital worlds can be constructed.</p>
         <p>This guide outlines how the Safe Network is constructed to achieve these aims. While it is somewhat technical in places, it's intended very much as an overview, and even those with very little technical knowledge should be able to gain a good working understanding of the Safe Network. For those requiring more depth, there are plenty of pointers as to where they can find the relevant information.</p>
        
       
@@ -21,14 +22,13 @@ const component = () => (
             <br /><a href='#ch3'>3. Nodes and Clients</a>
             <br /><a href='#ch4'>4. The architecture of the Safe Network</a>
             <br /><a href='#ch5'>5. Node Age</a>
-            <br /><a href='#ch6'>6. Encryption</a>
+            <br /><a href='#ch6'>6. Encryption and authentication</a>
             <br /><a href='#ch7'>7. Network-wide consensus not required </a>
-            <br /><a href='#ch8'>8. Farming for Safe Network Token</a>
-            <br /><a href='#ch9'>9. Node personas</a>
+            <br /><a href='#ch8'>8. Safe Network Token and Digital Bearer Certificates</a>
+            <br /><a href='#ch9'>9. Safe Network Anti-Entropy</a>
             <br /><a href='#ch10'>10. Data types</a>
-            <br /><a href='#ch11'>11. The Safe API</a>
-            <br /><a href='#ch12'>12. How Safe Network defends against common types of cyberattack</a>
-            <br /><a href='#ch13'>13. The promise of the Safe Network</a>
+            <br /><a href='#ch11'>11. How Safe Network defends against common types of cyberattack</a>
+            <br /><a href='#ch12'>12. The promise of the Safe Network</a>
         </p>
                             
 
