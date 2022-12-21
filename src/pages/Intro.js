15c5c9892b03e5f65ac7d9ef8de0ddefcce660af
JPL1 <JPL1@users.noreply.github.com>
2022-12-21 16:43:07 +0000

    Update Intro.js

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,13 +4,17 @@ const component = () => (
     <div className='intro'>
         <h2 id='ch0'>A brief introduction</h2>
   
-        <p>Technological progress is a perpetual process of automation and abstraction. Difficult and complex tasks are made simple by software and machines until they are completely taken for granted. Technology moves on, taking down new blockers in the path to progress as it goes. A good example of this is cloud computing, where once complex server administration tasks have been replaced, from the point of view of the consumer, by point-and-click.</p>
-        <p>The Safe (Secure Access For Everyone) Network takes this further, automating the entire network of interconnected machines, making it autonomous, secure, anonymous and capable of&nbsp; storing, protecting and delivering data without any human involvement at all.</p>
-        <p>The Safe Network is the vision of MaidSafe, a Scottish software company working in the field of decentralized computer networking. It is an autonomous peer-to-peer network created by linking together users' computers and smartphones that's designed to solve many of the current technical, managerial and societal problems exacerbated by centralized networks: a lack of privacy and data security, censorship and the massive consolidation of control by a few powerful actors.&nbsp;</p>
+      <p>Technological progress is a perpetual process of automation and abstraction. Difficult and complex tasks are made simple by software and machines until they are completely taken for granted. Technology moves on, taking down new blockers in the path to progress as it goes. A good example of this is cloud computing, where once complex server administration tasks have been replaced, from the point of view of the consumer, by point-and-click.</p>
 
-        <p>The Network is &lsquo;trustless&rsquo;, with no central point of control and no single point of failure. With connectivity and security taken care of, the Network is simple from the point of view of developers, with the burden of having to worry about low-level storage, networking, backups and computing resources removed. For people using the Network to store and share data and messages the burden of ensuring the security of precious information would be drastically reduced.</p>
-        <p>Anyone with a connected device can use the Network anonymously to store data or peruse public information on it, and anyone (subject to a resource test) can join the Network anonymously as a provider.</p> 
-        <p>The Safe Network is a platform on which new digital worlds can be constructed.</p>
+<p>The Safe Network—and its underlying protocol—takes this further, automating the entire network of interconnected machines, making it autonomous, secure, and capable of storing, protecting and delivering data without any human involvement at all.</p>
+
+<p>The Safe Network is the vision of MaidSafe, a Scottish software company working in the field of decentralized computer networking. It is an autonomous peer-to-peer network, created by linking together users’ computers and everyday connected devices, that’s designed to solve many of the current technical, managerial and societal problems exacerbated by centralized networks: a lack of privacy and data security, inefficiency and waste, censorship, and the massive consolidation of control by a few powerful actors.</p>
+
+<p>The Network is ‘trustless’, with no central point of control and no single point of failure. With connectivity and security taken care of, the Network is simple from the point of view of developers, with the burden of having to worry about low-level storage, networking, backups and computing resources removed. For people using the Network to store and share data and messages the burden of ensuring the security of precious information would be drastically reduced.</p>
+
+<p>Anyone with a connected device can use the Network to store and retrieve their own data, or access public information on it, and anyone (subject to a resource test) can join the Network as a supplier.</p>
+
+<p>The Safe Network is a platform on which new digital worlds can be constructed.</p>
         <p>This guide outlines how the Safe Network is constructed to achieve these aims. While it is somewhat technical in places, it's intended very much as an overview, and even those with very little technical knowledge should be able to gain a good working understanding of the Safe Network. For those requiring more depth, there are plenty of pointers as to where they can find the relevant information.</p>
        
       
