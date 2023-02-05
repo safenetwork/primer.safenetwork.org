0e51431e4a18a625adfca2adcc274eaaf295da55
JPL1 <JPL1@users.noreply.github.com>
2023-02-05 23:58:28 +0000

    Update Chap4.js

--- src/pages/Chap4.js
+++ src/pages/Chap4.js
@@ -18,7 +18,7 @@ const Chap4 = () => (
         <div className="What-does-that-mean">
             <h3>What does that mean?</h3>
             <p>Distributed hash table &ndash; a map of where data is stored on a distributed network.</p>
-            <p>XOR networking &ndash; a way of randomizing the physical location of data on a distributed network and ensuring each location is unique.</p>
+            <p>XOR networking &ndash; a way of randomizing the physical distribution of data on a distributed network and ensuring each location is unique.</p>
             <p>Section &ndash; a subset of addresses on the network. The word Section is also applied to a group of Nodes that manage a subset of addresses.</p>
             <p>Supermajority &ndash; larger than a simple majority, so e.g 5 out of 7 rather than 4 out of 7.</p>
             <p>Elders &ndash; Nodes with decision-making powers, the oldest 7 Nodes in a Section (see Chapter 5).</p>
@@ -36,7 +36,7 @@ const Chap4 = () => (
        <p>Importantly, on joining or rejoining the Safe Network a Node cannot simply pick its own XOR address - in other words, it can&rsquo;t choose the Section it will be part of. Instead, it has to wait to be allocated to a Section that is chosen by the Network. When this occurs, the Node receives the Section's Routing Table and learns the exact unique address range (and therefore the data) that it will be responsible for.</p>
         <p>Content stored on the Safe Network is first broken into chunks, hashed and then encrypted (see Chapter 6). Those chunks are run through a hashing algorithm to create a unique 256-bit hash for each chunk. Only chunks that are exactly identical will have the same hash value. This hash serves as the XOR address on the Network where that chunk will be stored, which in turn determines the Section that will manage it.</p>
         <p>Chunks with hashes that lie within a certain address range (say 000010... to 000011...) will be secured, stored and managed by the Section that is closest (in XOR terms) to that address. The membership of this Section will change over time as new Nodes join and others leave.</p>
-        <p>When the Network starts up, the initial Nodes are responsible for the whole 256-bit address range. Then as more Nodes join the Network will split into two Sections. As still more Nodes join, the Sections will continue to split, each managed autonomously by its Elders (Nodes with decision-making powers, see Chapter 5). This is known as &lsquo;Sharding&rsquo; and is a crucial part of the way in which the Network solves the challenges of scalability.</p>
+        <p>When the Network starts up, the initial Nodes are responsible for the whole 256-bit address range. There is one Section. Then as more Nodes join the Network will split into two Sections. As still more Nodes join, the Sections will continue to split, each managed autonomously by its Elders (Nodes with decision-making powers, see Chapter 5). This is known as &lsquo;Sharding&rsquo; and is a crucial part of the way in which the Network solves the challenges of scalability.</p>
         <p className="Pullquote">"The hash of a chunk of data serves as the XOR address on the Network where it will be stored, which in turn determines the Section that will manage it"</p>
         <div className="Full-width-pic" align="center">
 
@@ -44,9 +44,9 @@ const Chap4 = () => (
         </div>
         <p><em>Each Section (range of XOR addresses) is managed by a group of Nodes. The most trusted Nodes in a Section are called Elders. Elders have voting rights and can also communicate with Elders in other Sections.</em></p>
         <p> Experiments are ongoing to find the optimum number of Nodes per Section, but it will probably be around 100.</p>
-        <p>If a particular Section grows significantly in the eyes of the Network, it will split into two smaller Sections. </p>
+        <p>If a particular Section grows beyond a certain size, it will split into two smaller Sections.</p>
         <p>
-            If a Section finds its membership dropping below a certain limit (group_size) then it requests that Nodes be relocated to it earlier or that it has priority for Infant nodes joining the network. Given these balancing mechanisms is extremely unlikely that a Section will become too small (~7 nodes) to be viable.</p>
+            If a Section finds its membership dropping below a certain limit (group_size) then it requests that Nodes be relocated to it earlier or that it has priority for Infant Nodes joining the network. Given these balancing mechanisms is extremely unlikely that a Section will become too small (~7 Nodes) to be viable.</p>
 
 
    
@@ -54,7 +54,7 @@ const Chap4 = () => (
         <h3>Agreement</h3>
         <p>The Elder Nodes managing a Section must reach agreement among themselves on Network events under their control before they can happen. They also ‘Section-sign’ messages that travel over the wider Network. Elders in Sections receiving these messages need to be sure they are from valid Sections. Because Section membership is fluid, every time it changes (Elder churn) a new public key is created and signed by the current Elders using the previous key. This series of public keys, each signed by the previous one, is called a SectionTree and serves to prove the validity of the Section (See BLS-DKG, Anti-Entropy, Chapter 7). </p>
 
-        <p>SectionTrees allow Section authority to be baked into the data management process, providing a simple decentralized record of which Section signed which data.</p>
+        <p>The SectionTree allows Section authority to be baked into the data management process, providing a simple decentralized record of which Section signed which data.</p>
 
         <div className="Keep-it-simple">
             <h3>Keep it simple!</h3>
@@ -63,7 +63,7 @@ const Chap4 = () => (
        
 
 
-        <p className="Pullquote">"No node in the Safe Network has a complete overview of the network"</p>
+        <p className="Pullquote">"No Node in the Safe Network has a complete overview of the network"</p>
         <p>The closer a Node is to a certain address space on the Safe Network, the more information it has about data that is stored at that address. And logically, the further away a Node is, the less information it has. The bigger the Network becomes, the more secure it will get because an individual Node will have influence over a decreasing range of addresses.
                 </p>
         <p>An action or event happening in a Section is only valid once a supermajority of Elders (5 out of 7) has approved it. </p>
@@ -72,7 +72,6 @@ const Chap4 = () => (
         <p><a href="http://www.globule.org/publi/SDST_acmcs2009.html">A Survey of DHT Security Techniques (Globule)</a></p>
         <p><a href="https://pdos.csail.mit.edu/~petar/papers/maymounkov-kademlia-lncs.pdf ">Kademlia: A Peer-to-peer information system based on the XOR Metric (Whitepaper, 2010)</a></p>
         <p><a href="http://docs.maidsafe.net/Whitepapers/pdf/MaidSafeDistributedHashTable.pdf ">Distributed Hash Table (MaidSafe Whitepaper, 2010)</a></p>
-        <p><a href="http://docs.maidsafe.net/Whitepapers/pdf/DHTbasedNATTraversal.pdf ">DHT-based NAT Traversal (MaidSafe Whitepaper)</a></p>
         <p><a href="https://en.wikipedia.org/wiki/Disjoint-set_data_structure ">Disjoint-set data structure (Wikipedia)</a></p>
         <p><a href="https://github.com/maidsafe/rfcs/blob/master/text/0037-disjoint-groups/0037-disjoint-groups.md ">Disjoint Sections (MaidSafe RFC)</a></p>
 <p><a href="https://safenetforum.org/t/fast-ephemeral-routing/32173">Fast Ephemeral Routing (RFC and discussion, introduces concept of SectionChains - now SectionTrees)</a></p> 
